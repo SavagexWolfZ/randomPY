f91a2ac08ac4c4579a5108a246e691660ab94104
SavagexWolfZ <77026914+SavagexWolfZ@users.noreply.github.com>
2021-09-28 17:43:22 -0400

    Update randompassword.py

--- randompassword.py
+++ randompassword.py
@@ -11,8 +11,18 @@ password = ''.join(choice(all) for i in range(8))
 password2 = ''.join(choice(all) for i in range(8))
 password3 = ''.join(choice(all) for i in range(8))
 
+password4 = ''.join(choice(all) for i in range(10))
+password5 = ''.join(choice(all) for i in range(20))
+password6 = ''.join(choice(all) for i in range(30))
+
 print('HERE ARE 3 DIFFERENT PASSWORDS')
 
 print(password)
 print(password2)
 print(password3)
+
+print('HERE ARE SOME LONGER PASSWORDS')
+
+print(password4)
+print(password5)
+print(password6)
