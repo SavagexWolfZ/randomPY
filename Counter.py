8247ebe9e85475b1ef3d5038b4d43cc015d9d218
SavagexWolfZ <77026914+SavagexWolfZ@users.noreply.github.com>
2021-09-28 15:50:36 -0400

    Create Counter.py

---
+++ Counter.py
@@ -0,0 +1,16 @@
+from time import sleep
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+n = 1
+while True:
+  print(n)
+  n = n + 1
+  sleep(1)
+  
+
+//This counts 1 time every second.
+//Change sleep(1) to change the speed
