421c1b69455b233b470413fff110996183587c23
SavagexWolfZ <77026914+SavagexWolfZ@users.noreply.github.com>
2021-09-29 06:16:19 -0400

    Update randompassword.py

--- randompassword.py
+++ randompassword.py
@@ -5,34 +5,28 @@ characters = 'abcdefghijklmnopqrstuvwxyz'
 up = characters.upper()
 specialCharacters = '-_!?&$%'
 
-all = digits+specialCharacters+characters+up
+everything = digits+specialCharacters+characters+up
 
-password = ''.join(choice(all) for i in range(8))
-password2 = ''.join(choice(all) for i in range(8))
-password3 = ''.join(choice(all) for i in range(8))
+password = ''.join(choice(everything) for i in range(8))
+password2 = ''.join(choice(everything) for i in range(8))
+password3 = ''.join(choice(everything) for i in range(8))
 
-password4 = ''.join(choice(all) for i in range(15))
-password5 = ''.join(choice(all) for i in range(20))
-password6 = ''.join(choice(all) for i in range(30))
+password4 = ''.join(choice(everything) for i in range(15))
+password5 = ''.join(choice(everything) for i in range(20))
+password6 = ''.join(choice(everything) for i in range(30))
 
-password100 = ''.join(choice(all) for i in range(100))
-password200 = ''.join(choice(all) for i in range(100))
-password300 = ''.join(choice(all) for i in range(100))
+password100 = ''.join(choice(everything) for i in range(100))
+password200 = ''.join(choice(everything) for i in range(100))
+password300 = ''.join(choice(everything) for i in range(100))
 
-print('HERE ARE 3 DIFFERENT PASSWORDS')
+print('HERE ARE 3 DIFFERENT PASSWORDS' + '\n' + '==========' + '\n')
 
-print(password)
-print(password2)
-print(password3)
+print(password+ '\n' + password2 + '\n' + password3 + '\n')
 
-print('HERE ARE SOME LONGER PASSWORDS')
+print('HERE ARE SOME LONGER PASSWORDS' + '\n' + '==========' + '\n')
 
-print(password4)
-print(password5)
-print(password6)
+print(password4 + '\n' + password5 + '\n' + password6+ '\n')
 
-print('HERE ARE SOME 100 DIGIT PASSWORDS')
+print('HERE ARE SOME 100 DIGIT PASSWORDS' + '\n' + '==========' + '\n')
 
-print(password100)
-print(password200)
-print(password300)
+print(password100 + '\n\n' + password200 + '\n\n' + password300 + '\n\n')
